beba64356b99a1cbed1cc3f64d5ed152c796c260
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-04-27 12:48:18 +0600

    dynamic id

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+const users = [
+    {id:1, name: 'khalek' },
+    {id:2, name: 'falek' },
+    {id:3, name: 'galek' },
+    {id:4, name: 'palek' },
+    {id:5, name: 'malek' },
+]
+
+
 app.get('/', (req, res)=>{
-    res.send("look, this is node")
+    res.send("look, this  is my node")
 }); 
+
+app.get('/users', (req, res)=>{
+    res.send(users)
+});
+
+app.get('/user/:id', (req, res)=>{
+    console.log(req.params);
+    const id = req.params.id;
+    const user = users.find(user=>user.id ==id);
+    res.send(user)
+});
 
 app.listen(port, ()=>{
     console.log('listening to port', port);
+
 })
